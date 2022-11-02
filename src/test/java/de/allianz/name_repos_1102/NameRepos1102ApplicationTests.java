fe6ef1103e2947a1b402543a5ffe06b23f24002e
CaiLiu <liucaissr@gmail.com>
2022-11-02 13:32:17 +0100

    Create maven project

---
+++ src/test/java/de/allianz/name_repos_1102/NameRepos1102ApplicationTests.java
@@ -0,0 +1,13 @@
+package de.allianz.name_repos_1102;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NameRepos1102ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
