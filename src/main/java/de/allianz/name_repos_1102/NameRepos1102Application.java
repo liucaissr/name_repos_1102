8465939dc74cb3f4d4f9c6186de3b19c4b3c565a
CaiLiu <liucaissr@gmail.com>
2022-11-02 14:24:33 +0100

    Create maven project

---
+++ src/main/java/de/allianz/name_repos_1102/NameRepos1102Application.java
@@ -0,0 +1,13 @@
+package de.allianz.name_repos_1102;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NameRepos1102Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NameRepos1102Application.class, args);
+	}
+
+}
